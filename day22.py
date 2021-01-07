77461d07c621e36d54e09fa65aaa8b16c4f388a6
Richard Taylor <29562583+richardt94@users.noreply.github.com>
2021-01-07 14:40:01 +1100

    day 22 part 2

--- day22.py
+++ day22.py
@@ -21,23 +21,49 @@ while deck[i] != 2019:
     i += 1
 print(i)
 
-#part 2
-def inidx(i, decksize):
-    #trace the path backwards
-    for ins in instrs[::-1]:
-        if ins.startswith("deal into new stack"):
-            i = decksize - i - 1
-        elif ins.startswith("deal with increment"):
-            inc = int(ins.split()[-1])
-            m = 0
-            while (m * decksize + i)%inc != 0:
-                m += 1
-            i = (m * decksize + i) // inc
-        elif ins.startswith("cut"):
-            n = int(ins.split()[-1])
-            i = (i + n) % decksize
-    return i
+#use modular arithmetic to do the thing
 
+with open("inputday22.txt") as f:
+    instrs = f.readlines()
+
+offset = 0
+inc = 1
 ds = 119315717514047
-ii = 2020
-print(inidx(ii, ds))
+
+for ins in instrs:
+    if ins.startswith("deal into new stack"):
+        offset -= inc
+        offset %= ds
+        inc = ds - inc
+    elif ins.startswith("deal with increment"):
+        #replace with non-brute-force for actual input
+        dinc = int(ins.split()[-1])
+        i = 0
+        while (i*ds + 1) % dinc:
+            i += 1
+        j = (i*ds + 1)//dinc
+        inc = (inc * j) % ds
+    elif ins.startswith("cut"):
+        n = int(ins.split()[-1])
+        offset += n * inc
+        offset %= ds
+
+#exponentiate by squaring
+ntimes = 101741582076661
+ioffset = offset
+iinc = inc
+offset = 0
+inc = 1
+while ntimes > 0:
+    nsquares = 1
+    noffset = ioffset
+    ninc = iinc
+    while (2**nsquares < ntimes):
+        noffset = (noffset + noffset*ninc) % ds
+        ninc = (ninc * ninc) % ds
+        nsquares += 1
+    offset = (offset + noffset*inc) % ds
+    inc = (inc * ninc) % ds
+    ntimes -= 2**(nsquares - 1)
+
+print((offset + 2020 * inc) % ds)
